654369b7486412031f026ae992becfcdda6ac986
Attie Grande <attie@attie.co.uk>
2019-09-29 18:00:15 +0100

    rearranged the colors for rainbow ordering, and removed off / white

--- color.c
+++ color.c
@@ -3,21 +3,21 @@
 
 inline void color_tx(enum state_color color) {
 	/* blue */
-	if ((color == COL_BLUE) || (color == COL_CYAN) || (color == COL_MAGENTA) || (color == COL_WHITE)) {
+	if ((color == COL_BLUE) || (color == COL_CYAN) || (color == COL_MAGENTA)) {
 		CHAIN_DATA_1();
 	} else {
 		CHAIN_DATA_0();
 	}
 
 	/* green */
-	if ((color == COL_GREEN) || (color == COL_YELLOW) || (color == COL_CYAN) || (color == COL_WHITE)) {
+	if ((color == COL_GREEN) || (color == COL_YELLOW) || (color == COL_CYAN)) {
 		CHAIN_DATA_1();
 	} else {
 		CHAIN_DATA_0();
 	}
 
 	/* red */
-	if ((color == COL_RED) || (color == COL_YELLOW) || (color == COL_MAGENTA) || (color == COL_WHITE)) {
+	if ((color == COL_RED) || (color == COL_YELLOW) || (color == COL_MAGENTA)) {
 		CHAIN_DATA_1();
 	} else {
 		CHAIN_DATA_0();
--- color.h
+++ color.h
@@ -2,14 +2,13 @@
 #define COLOR_H
 
 enum state_color {
-	COL_OFF,     /* red green blue */
+	             /* red green blue */
 	COL_RED,     /*  1    0    0   */
+	COL_YELLOW,  /*  1    1    0   */
 	COL_GREEN,   /*  0    1    0   */
 	COL_BLUE,    /*  0    0    1   */
-	COL_YELLOW,  /*  1    1    0   */
 	COL_CYAN,    /*  0    1    1   */
 	COL_MAGENTA, /*  1    0    1   */
-	COL_WHITE,   /*  1    1    1   */
 	COL_EOL,
 };
 
