12bb9bb079de151607d98aa34fcc49f03ebd0357
Attie Grande <attie@attie.co.uk>
2019-10-01 11:01:04 +0100

    fixed blue/cyan color order

--- color.h
+++ color.h
@@ -6,8 +6,8 @@ enum state_color {
 	COL_RED,     /*  1    0    0   */
 	COL_YELLOW,  /*  1    1    0   */
 	COL_GREEN,   /*  0    1    0   */
-	COL_BLUE,    /*  0    0    1   */
 	COL_CYAN,    /*  0    1    1   */
+	COL_BLUE,    /*  0    0    1   */
 	COL_MAGENTA, /*  1    0    1   */
 	COL_EOL,
 };
